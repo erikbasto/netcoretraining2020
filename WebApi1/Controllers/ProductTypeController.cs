120c6eb4211fbee3a74457be0b40a7b629020531
erikbasto <erik.basto85@gmail.com>
2020-09-02 16:58:38 -0500

    Product Type Contoller

---
+++ WebApi1/Controllers/ProductTypeController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using AT.IDataAccess.IRepositoryPattern;
+using AT.Model.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductTypeController: ControllerBase
+    {
+        private readonly IRepository<ProductType> _productTypeRepository;
+
+        public ProductTypeController(IRepository<ProductType> productTypeRepository)
+        {
+            _productTypeRepository = productTypeRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductType>> GetAll()
+        {
+            return Ok(_productTypeRepository.GetAll());
+        }
+
+        [HttpGet("Id")]
+        public ActionResult<ProductType> GetById(int Id)
+        {
+            return Ok(_productTypeRepository.GetById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult<ProductType> NewProductType(ProductType productType)
+        {
+            return Ok(_productTypeRepository.Create(productType));
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteProductType(ProductType productType){
+            _productTypeRepository.Delete(productType);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult<ProductType> EditProductType(ProductType productType)
+        {
+            try
+            {
+                return Ok(_productTypeRepository.Update(productType));
+            }
+            catch(ArgumentNullException )
+            {
+                return BadRequest();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
